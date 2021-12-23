4f682f6b4f275e96d2f9b50d1e92d3edec80ab45
zkffhtm6523 <zkffhtm6523@gmail.com>
2021-12-23 17:30:07 +0900

    TodoList 리스트 추가 기능 연동

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,11 +1,30 @@
 <template>
-  <div>list</div>
+  <section>
+    <ul>
+      <li v-for="todoItem in todoItems">{{ todoItem }}</li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
-  name: "TodoList"
+  data(){
+    return {
+      todoItems: []
+    }
+  },
+  created(){
+    if(localStorage.length > 0){
+      for (var i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(localStorage.key(i));
+        }
+      }
+    }
+    console.log(`todoItem : ${this.todoItems}`)
+  }
 }
+
 </script>
 
 <style scoped>
