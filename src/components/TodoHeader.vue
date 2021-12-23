948d722ee20faf45cd778e25f8d64b14987a5c5c
zkffhtm6523 <zkffhtm6523@gmail.com>
2021-12-23 15:53:45 +0900

    TodoHeader 속성 추가

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>header</div>
+  <h1>TODO it!</h1>
 </template>
 
 <script>
@@ -7,7 +7,11 @@ export default {
   name: "TodoHeader"
 }
 </script>
-
+<!-- scope : 스타일 정의를 해당 컴포넌트에만 적용하겠다는 의미-->
 <style scoped>
-
+  h1 {
+    color: #2f3b52;
+    font-weight: 900;
+    margin: 2.5rem 0 1.5rem;
+  }
 </style>
