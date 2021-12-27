7d45e83e9a2319e249f5dbd5d37cd7375282e7e4
zkffhtm6523 <zkffhtm6523@gmail.com>
2021-12-27 23:15:15 +0900

    Delete 기능 추가

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,13 @@
 <template>
   <section>
     <ul>
-      <li v-for="todoItem in todoItems">{{ todoItem }}</li>
+      <li v-for="(todoItem, index) in todoItems" :key="todoItem" class="shadow">
+        <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+        {{ todoItem }}
+        <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+          <i class="far fa-trash-alt" aria-hidden="true"></i>
+        </span>
+      </li>
     </ul>
   </section>
 </template>
@@ -22,11 +28,45 @@ export default {
       }
     }
     console.log(`todoItem : ${this.todoItems}`)
+  },
+  methods:{
+    removeTodo(todoItem, index){
+      console.log(todoItem, index);
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
   }
 }
 
 </script>
 
 <style scoped>
+  ul {
+    list-style-type: none;
+    padding-left: 0;
+    margin: 0;
+    text-align: left;
+  }
 
+  li {
+   display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+  }
+
+  .checkBtn{
+    line-height: 45px;
+    color: #62acde;
+    margin-left: 5px;
+  }
+
+  .removeBtn{
+    margin-left: auto;
+    color: #de4343;
+  }
 </style>
